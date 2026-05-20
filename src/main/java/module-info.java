d85347c21b6dd80807c3585c520fd78632755e42
Andres Camilo Carvajal Figueroa <andresc.carvajalf@uqvirtual.edu.co>
2026-05-19 22:21:28 -0500

    Primer Commit del Proyecto Final

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module co.edu.uniquindio.techpark420.proyectofinalgestiontechparkuq {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens co.edu.uniquindio.techpark420.proyectofinalgestiontechparkuq to javafx.fxml;
+    exports co.edu.uniquindio.techpark420.proyectofinalgestiontechparkuq;
+}
