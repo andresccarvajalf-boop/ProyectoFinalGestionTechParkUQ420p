50840d209e37054025f7e677f4874916cd1f6c66
Andres Camilo Carvajal Figueroa <andresc.carvajalf@uqvirtual.edu.co>
2026-05-23 11:43:31 -0500

    Por ahora creo que es todo Espero...

--- src/main/java/co/edu/uniquindio/techpark420/proyectofinalgestiontechparkuq/HelloApplication.java
+++ src/main/java/co/edu/uniquindio/techpark420/proyectofinalgestiontechparkuq/HelloApplication.java
@@ -4,6 +4,7 @@ import co.edu.uniquindio.techpark420.proyectofinalgestiontechparkuq.app.AppConte
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -15,9 +16,14 @@ public class HelloApplication extends Application {
         AppContext.getInstance().setStagePrincipal(stage);
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource(
-        "/co/edu/uniquindio/techpark420/proyectofinalgestiontechparkuq/views/login-view.fxml"
+            "/co/edu/uniquindio/techpark420/proyectofinalgestiontechparkuq/views/login-view.fxml"
         ));
         StackPane root = loader.load();
+
+        Image icono = new Image(getClass().getResourceAsStream(
+            "/co/edu/uniquindio/techpark420/proyectofinalgestiontechparkuq/imgs/001.png"
+        ));
+        stage.getIcons().add(icono);
 
         Scene scene = new Scene(root, 1024, 700);
         stage.setTitle("Tech-Park UQ");
@@ -25,10 +31,11 @@ public class HelloApplication extends Application {
         stage.setResizable(false);
         stage.show();
     }
+
     @Override
-public void stop() {
-    AppContext.getInstance().guardarDatos();
-}
+    public void stop() {
+        AppContext.getInstance().guardarDatos();
+    }
 
     public static void main(String[] args) {
         launch(args);
