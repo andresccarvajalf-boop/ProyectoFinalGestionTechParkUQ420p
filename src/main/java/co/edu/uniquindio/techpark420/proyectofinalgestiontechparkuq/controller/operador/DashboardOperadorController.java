cbde9a5c5db12bda91b78dc585e1464b570b5dbc
Andres Camilo Carvajal Figueroa <andresc.carvajalf@uqvirtual.edu.co>
2026-05-25 23:54:00 -0500

    Mi documentacion xd

--- src/main/java/co/edu/uniquindio/techpark420/proyectofinalgestiontechparkuq/controller/operador/DashboardOperadorController.java
+++ src/main/java/co/edu/uniquindio/techpark420/proyectofinalgestiontechparkuq/controller/operador/DashboardOperadorController.java
@@ -1,3 +1,11 @@
+/*
+Universidad del Quindío - Facultad de Ingeniería
+Autor: Andres Camilo Carvajal Figueroa C.C 1066602456
+Profesor: Ing. Jhan Carlos Martínez Ceballos 
+2026-05
+GNU GPL
+*/
+
 package co.edu.uniquindio.techpark420.proyectofinalgestiontechparkuq.controller.operador;
 
 import java.net.URL;
